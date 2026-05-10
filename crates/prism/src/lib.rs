49875217070ce2e6f61e657820079ebf414df9a4
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2026-05-11 00:53:54 +0800

    refactor(prism/lib): remove motd and template modules

--- crates/prism/src/lib.rs
+++ crates/prism/src/lib.rs
@@ -3,14 +3,14 @@ pub mod config;
 mod context;
 pub mod hooks;
 pub mod inbound;
-pub mod motd;
+
 pub mod network;
 pub mod outbound;
 pub mod players;
 pub mod relay;
 pub mod session;
 pub mod stats;
-pub mod template;
+
 mod transport;
 
 pub use config::Config;
